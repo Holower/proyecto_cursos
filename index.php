81a30fe1d5ded20da3a426ef1eb4c3af042fa605
Holower <camiloandresjs@ufps.edu.co>
2020-11-08 23:21:11 -0500

    Muchos cambios
    Co-Authored-By: null <Holower@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$alert = '';
+if (!empty($_POST))
+{
+  echo $alert="le dio click en log in";
+}
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,17 +13,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon" type="icon/png" href="https://avatars3.githubusercontent.com/u/23146203?s=460&u=a133e2cd19a0b7f37e3e267dea29cbdf3b5ce172&v=4">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="assets/css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
 	<?php require 'partials/header.php' ?>
 	<div>
-		<img src="images/logo_vertical.png">
+		<img src="img/logo_vertical.png">
 	</div>
 	<div>
 		<h2>Login</h2>
 		<!-- Login Form -->
-		<form>
+		<form action="" method="post">
 			<input type="text" id="login" name="email" placeholder="Enter your email">
 			<input type="password" id="password" name="password" placeholder="Enter your password">
 			<input type="submit" value="Log In">
